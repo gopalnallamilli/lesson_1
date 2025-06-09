e7d4c0d57467ad7ed7a445ef94d232b4fd94a7a3
gopalnallamilli <gnallamilli123@gmail.com>
2025-06-09 15:47:29 -0400

    demo

--- activty1.py
+++ activty1.py
@@ -1,11 +1,1 @@
-# Printing a greeting message
-print ("Hello, everyone")
-
-# printing my name
-print("My name is gopal")
-
-# Printing a fun fact abt myself
-print("I do taek won do")
-
-# Preforming and printing a simple calculation
-print("The result of 5 + 3 is:", 3 + 5)
+t
